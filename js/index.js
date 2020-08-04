0b1f8dfed2613357568e72afea5e3efd50456369
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-04 19:30:26 +0530

    Added Theme Switcher

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 const html = document.getElementsByTagName('html')[0];
-const toggleTheme = (theme) => {
-    html.dataset.theme = theme;
-}
 
-toggleTheme("dark")
+mainTheme = "light";
+
+const toggleTheme = () => {
+    if (mainTheme === "light"){
+        mainTheme = "dark"
+        html.dataset.theme = mainTheme;
+        console.log("Setting Theme to ", mainTheme);
+    }
+
+    else {
+        mainTheme = "light"
+        html.dataset.theme = mainTheme;
+        console.log("Setting Theme to ", mainTheme);
+    }
+}
